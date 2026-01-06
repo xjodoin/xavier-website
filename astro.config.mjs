8585cf55aac56417bc11de818f06890fa5482cbd
Xavier Jodoin <xavier.jodoin@greybox.biz>
2026-01-05 22:39:18 -0500

    githubb action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+const site = process.env.PUBLIC_SITE ?? 'https://xavierjodoin.com';
+const base = process.env.PUBLIC_BASE ?? '/';
+
 export default defineConfig({
-  site: 'https://xavierjodoin.com',
+  site,
+  base,
   scopedStyleStrategy: 'where',
   markdown: {
     syntaxHighlight: 'prism'
